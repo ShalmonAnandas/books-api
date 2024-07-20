5b059b0ddda7dc1e2341ced61668b6b05a46b613
ShalmonAnandas <shalmona@snapwork.com>
2024-07-21 00:45:47 +0530

    vercel deployment changes

--- src/index.py
+++ src/index.py
@@ -14,27 +14,27 @@ libgen = LibgenSearch()
 
 
 @app.get("/")
-def read_root():
-    return "Welcome to Books API"
+async def root():
+    return {"message" : "Welcome to Books API"}
 
 
 @app.post("/book")
-def getsearchResults(query: QueryModel):
+async def getsearchResults(query: QueryModel):
     return searchBook(query.search_query, False)
 
 
 @app.post("/author")
-def getSearchResultsByAuthor(query: QueryModel):
+async def getSearchResultsByAuthor(query: QueryModel):
     results = searchBook(query.search_query, True)
     return results
 
 
 @app.post("/links")
-def getDownloadLinks(item: Book):
+async def getDownloadLinks(item: Book):
     return getDownload(item)
 
 
-def searchBook(searchTerm: str, isSearchAuthor: bool):
+async def searchBook(searchTerm: str, isSearchAuthor: bool):
     if isSearchAuthor:
         results = libgen.search_author(searchTerm.replace("%20", " "))
     else:
@@ -46,9 +46,5 @@ def searchBook(searchTerm: str, isSearchAuthor: bool):
     return tempList
 
 
-def getDownload(downloadLink: Book):
+async def getDownload(downloadLink: Book):
     return libgen.resolve_download_links(downloadLink.to_dict())
-
-
-if __name__ == "__main__":
-    uvicorn.run(app)
